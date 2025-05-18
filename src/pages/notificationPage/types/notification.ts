b371544969d63daf215b6e0bd91eaea622483a0f
Joseph Park <rkfnahs12e@gmail.com>
2025-05-18 15:52:55 +0900

    design : notification 알림 ui 구현

---
+++ src/pages/notificationPage/types/notification.ts
@@ -0,0 +1,10 @@
+export type notification = {
+  id: number;
+  type: string;
+  is_read: boolean;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  receiver_id: string;
+  sender_id: string;
+};
--- src/pages/notificationPage/ui/NotificationPage.tsx
+++ src/pages/notificationPage/ui/NotificationPage.tsx
@@ -1,3 +1,28 @@
+import Button from '../../../widgets/button/Button';
+
 export default function NotificationPage() {
-  return <div>Notification Page</div>;
+  return (
+    <div className='max-w-[390px] mx-auto px-[5px]'>
+      <div className='bg-gradient-to-b from-[#cae4f7] to-[#d9c9e280] px-3 rounded-2xl'>
+        <div className='flex justify-between items-center w-full py-6'>
+          <span className='text-base'> 박요셉님이 과외를 신청하셨습니다.</span>
+          <span className='text-base text-[#999999]'>1시간 전</span>
+        </div>
+        <div className='w-full flex items-center justify-around px-14 pb-4'>
+          <Button
+            label='수락'
+            variant='outline'
+            theme='student'
+            className='w-30 bg-white h-[30px]'
+          />
+          <Button
+            label='거부'
+            variant='outline'
+            theme='teacher'
+            className='w-30 bg-white h-[30px]'
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
